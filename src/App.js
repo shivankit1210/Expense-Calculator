97e2d2d69e6a6b3ad9fdc83d871f79427ca97561
Shivankit Agarwal <shivankit1210@gmail.com>
2024-01-13 22:34:02 +0530

    add components in project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
-
+import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpense } from "./components/IncomeExpense";
+import { TransactionList } from "./components/TransactionList";
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+        <TransactionList/>
+      </div>
     </div>
   );
 }
---
+++ src/components/TransactionList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const TransactionList = () => {
+  return (
+    <div>
+        <h3>History</h3>
+        <ul className="list" id='list'>
+            <li className='minus'>
+                cash <span>-$400 </span>
+                <button className='delete-btn'>X</button>
+            </li>
+        </ul>
+    </div>
+  )
+}
